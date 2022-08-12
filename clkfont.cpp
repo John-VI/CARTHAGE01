1c4409b4a135242f071a35e6adb80d1b105afaec
John Whitley <dogjaw2233@gmail.com>
2022-08-12 01:56:40 -0400

    Deleted orphaned clkfont sources

--- clkfont.cpp
+++
@@ -1,3 +0,0 @@
-// Copyright(c) John Allen Whitley, 2022, BSD 3 - Clause
-
-#include "clkfont.h"
--- clkfont.h
+++
@@ -1,5 +0,0 @@
-// Copyright(c) John Allen Whitley, 2022, BSD 3 - Clause
-
-#pragma once
-
-class font {};
